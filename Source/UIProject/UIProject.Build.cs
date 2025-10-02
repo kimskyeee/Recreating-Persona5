cbada454b661509c7395d9a0d047a4efd1c5b53f
kimskyeee <zlagksnf@gmail.com>
2025-10-02 19:56:00 +0900

    Add: 초기 세팅
    Common UI 모듈, Input Data 추가

---
+++ Source/UIProject/UIProject.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UIProject : ModuleRules
+{
+	public UIProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"UIProject",
+			"UIProject/Variant_Platforming",
+			"UIProject/Variant_Combat",
+			"UIProject/Variant_Combat/AI",
+			"UIProject/Variant_SideScrolling",
+			"UIProject/Variant_SideScrolling/Gameplay",
+			"UIProject/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		PrivateDependencyModuleNames.AddRange(new string[]
+			{ "Slate", "SlateCore", "CommonUI", "CommonInput", "GameplayTags" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/UIProject/UIProjectGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UIProjectGameMode.h"
+
+AUIProjectGameMode::AUIProjectGameMode()
+{
+	// stub
+}
